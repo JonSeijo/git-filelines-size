bf7a81e2c7f81b3650a35f9b636a1253f3d37540
jonseijo <jon.seijo@gmail.com>
2017-02-04 14:18:24 -0300

    Add argument to specify external git repo

--- filelines.py
+++ filelines.py
@@ -3,14 +3,6 @@ import re
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filename", help = "Relative or absolute path to the file you want to measure")
-
-args = parser.parse_args()
-
-filename = args.filename
-
-
 outfile_git_name = "tmp_git.txt"
 outfile_format_name = "tmp_formatted.txt"
 
@@ -18,6 +10,30 @@ git_log_constant = "1 file changed, "  # Used for grep, do not modify
 
 diff_list = []
 total_list = []
+
+# Parse arguments passed by command line
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help = "Relative or absolute path to the file you want to measure")
+parser.add_argument("--gitdir",
+         help = "Specify repository directory if the file to measure is in a ouside repository. "
+            + "(Relative or absolute)")
+
+args = parser.parse_args()
+
+filename = args.filename
+
+#git "--git-dir=/home/repo/.git"
+command_git_log = "git"
+
+
+if args.gitdir != None:
+    command_git_log += " -C " + args.gitdir
+
+command_git_log += " log --stat --pretty=format: " + filename
+
+
+# python3 filelines.py --gitdir /home/jonno/exactas/algo2/tp3/.git /home/jonno/exactas/algo2/tp3/Base/Juego.cpp
+# python3 filelines.py --gitdir /home/jonno/exactas/algo2/tp3 /home/jonno/exactas/algo2/tp3/Base/Juego.cpp
 
 def is_insertion(str):
     return str == "insertion" or str == "insertions"
@@ -41,7 +57,7 @@ def get_stats(line):
 
 
 with open(outfile_git_name, "w") as outfile:
-    subprocess.call(["git", "log", "--stat",  "--pretty=format:", filename], stdout = outfile)
+    subprocess.call(command_git_log.split(), stdout = outfile)
 
 with open(outfile_format_name, "w") as outfile:
     subprocess.call(["grep", git_log_constant, outfile_git_name], stdout = outfile)
@@ -69,5 +85,5 @@ for i in range(1, len(diff_list)):
 plt.plot(total_list)
 plt.title(filename)
 plt.ylabel('File lines')
-plt.ylabel('Commits')
+plt.xlabel('Commits')
 plt.show()
