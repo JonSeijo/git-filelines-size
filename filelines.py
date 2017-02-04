842aa448473f682d8a64c5f1721170740d383750
jonseijo <jon.seijo@gmail.com>
2017-02-04 12:45:18 -0300

    Create diff list for file

--- filelines.py
+++ filelines.py
@@ -8,10 +8,28 @@ outfile_format_name = "tmp_formatted.txt"
 
 git_log_constant = "1 file changed, "  # Used for grep, do not modify
 
+diff_list = []
 
-def getStats(line):
+def is_insertion(str):
+    return str == "insertion" or str == "insertions"
+
+
+def get_stats(line):
     words = re.findall("\w+", line)
-    print(words)
+
+    current_diff = 0
+
+    # Magic number 4 to avoid "1 file changed" string
+    # Removing it add unnecesary extra operations when I can simply skip them 
+    # (Will only read them once)
+    for i in range(4, len(words), 2):
+        if is_insertion(words[i]):
+            current_diff += int(words[i-1])
+        else:
+            current_diff -= int(words[i-1])
+
+    diff_list.append(current_diff)
+    print(diff_list)
 
 
 with open(outfile_git_name, "w") as outfile:
@@ -27,6 +45,6 @@ subprocess.call(["rm", outfile_git_name])
 
 with open(outfile_format_name) as f:
     for line in f:
-        getStats(line)
+        get_stats(line)
 
 
