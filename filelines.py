2d0280ab8fa54ec9008d28cac6221bc20f030fcc
jonseijo <jon.seijo@gmail.com>
2017-02-04 12:23:07 -0300

    grep of line number changes

--- filelines.py
+++ filelines.py
@@ -1,4 +1,14 @@
 import subprocess
 filename = "README.md"
 
-subprocess.call(["git", "log", "--stat",  "--pretty=format:", filename])
+outfile_git_name = "tmp_git.txt"
+outfile_format_name = "tmp_formatted.txt"
+git_log_constant = "1 file changed, "
+
+with open(outfile_git_name, "w") as outfile:
+    subprocess.call(["git", "log", "--stat",  "--pretty=format:", filename], stdout = outfile)
+
+with open(outfile_format_name, "w") as outfile:
+    subprocess.call(["grep", git_log_constant, outfile_git_name], stdout = outfile)
+
+subprocess.call(["gedit", outfile_format_name])
